d348fe67df612144980c3a522771c50f36368d30
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-11-21 06:34:12 +0530

    updating ./terraform-practice

--- ex2/main.tf
+++ ex2/main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "allow_ssh" {
 resource "aws_instance" "web" {
   ami                    = "ami-0408d6aa07d74894b"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = aws_security_group.allow_ssh.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
 
   tags = {
