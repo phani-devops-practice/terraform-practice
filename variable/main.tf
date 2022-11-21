01be42ec3b185f47e1a7e21d0a02b457d42355a7
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-11-21 06:14:23 +0530

    updating ./terraform-practice

--- variable/main.tf
+++ variable/main.tf
@@ -8,3 +8,6 @@ output "xyz" {
 
 // their are 3 variable type. default variable, list variable and map variable.
 // Their are 3 types of variables. string type, boolean type and number
+
+output "abc" {
+  value = ${var.b}
